30e2d7fb9c9a427110635403a8fe8d9e716b00ee
Tareq Judeh <tareq.judeh@outlook.com>
2021-04-17 20:59:56 +0300

    Uploading course files and folders to GitHub.

---
+++ section2/section2.js
@@ -0,0 +1,22 @@
+// JS introduction:
+
+console.log("Hello, World!");
+let js = "Amazing";
+
+if (js == "Amazing") {
+     console.log("JS is AMAZING!");
+}
+
+if (js == "Amazing") alert ("JavaScript is AMAZING!");
+js = "Not amazing";
+js == "Amazing" ? console.log("JS is AMAZING!") : console.log("Or not.");
+
+/* 
+JS is a high level, OOP language.
+!High-level, like we don't have to worry about complex concepts like memory management.
+JS is a multi-paradigm language. Meaning, JS is flexible.
+
+*ES6 => Released in 2015
+
+*ES = Ecma Script
+*/
