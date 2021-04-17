5ef78d6fb59474a56cafdaf165c2192e5cd0e808
Tareq Judeh <tareq.judeh@outlook.com>
2021-04-17 21:18:08 +0300

    Created .gitignore file.

--- section2/01-Fundamentals-Part-1/starter/section2.js
+++ section2/01-Fundamentals-Part-1/starter/section2.js
@@ -10,7 +10,8 @@ if (js == "Amazing") {
 if (js == "Amazing") alert ("JavaScript is AMAZING!");
 js = "Not amazing";
 js == "Amazing" ? console.log("JS is AMAZING!") : console.log("Or not.");
-
+const result = 40+8-3/2
+console.log(result);
 /* 
 JS is a high level, OOP language.
 !High-level, like we don't have to worry about complex concepts like memory management.
